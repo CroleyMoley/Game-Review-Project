7160b839befb19d59e3f0b830af06a82f533b235
MisterAnthropy <tjcroley@outlook.com>
2020-10-13 12:34:17 -0700

    renamed user relationship with reviewed games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
     belongs_to :genre
     belongs_to :user
+    has_many :reviews 
+    has_many users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :reviewed_games, through: :reviews, source: :game
+    has_many :games
 end
