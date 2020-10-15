50e17e5c88a6866f44119751fe0386ea94e40d17
MisterAnthropy <tjcroley@outlook.com>
2020-10-15 15:06:51 -0700

    created form for login path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,13 @@ class SessionsController < ApplicationController
     def welcome
         
     end
+
+    def new 
+    end
+
+    def create
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user 
+    end
+
 end
