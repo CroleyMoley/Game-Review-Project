bedba29f00faa93af1755966de79177cba745e96
MisterAnthropy <tjcroley@outlook.com>
2020-10-17 16:21:17 -0700

    added nested route for reviews through games

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   delete '/logout' => 'sessions#destroy'
   resources :reviews
+  resources :games do 
+    resources :reviews, only: [:new, :index]
+  end
   resources :genres
-  resources :games
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
