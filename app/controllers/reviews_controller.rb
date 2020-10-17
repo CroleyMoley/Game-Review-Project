bdcc531ece09690baff296dc63fd64135f7336ba
MisterAnthropy <tjcroley@outlook.com>
2020-10-17 16:43:21 -0700

    added complete form for nested review route, added functionality to leave review through separate route also

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,15 @@
 class ReviewsController < ApplicationController
     def new
+        if @game = Game.find_by_id(params[:game_id])
+        @review = @game.reviews.build
+        else
+            @review = Review.build
+        end
     end
 
     def index
+        
     end
 
-    
+
 end
