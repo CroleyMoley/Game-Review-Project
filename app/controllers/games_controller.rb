0d67c8d9ad041effa1739b25ed8783cf368274f8
MisterAnthropy <tjcroley@outlook.com>
2020-10-17 16:02:31 -0700

    added index and show page for games that users add

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,8 +13,12 @@ class GamesController < ApplicationController
        end 
     end
 
-    def show 
+    def index 
+        @games = Game.all 
+    end
 
+    def show 
+        @game = Game.find_by_id(params[:id])
     end
 
     private
