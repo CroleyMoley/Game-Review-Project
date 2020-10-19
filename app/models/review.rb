f7f36a961ad4fbcfd7c7d73d60d08321d299e07e
MisterAnthropy <tjcroley@outlook.com>
2020-10-19 13:22:55 -0700

    added validation to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
 
-  validates :title, presence: true
+  validates :title, :content, presence: true
   validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 11}
-  validates :content, presence: true
+  
+  validates :game, uniqueness: { scope: :user }
 end
